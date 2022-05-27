ad154d0b44071848aba7a08f3eb6909fb01d4fce
Jaron Schoorlemmer <schoorja@flip1.engr.oregonstate.edu>
2022-05-27 12:21:26 -0700

    added queries/reqests to load & add pokemon to Pokemon page

--- app.js
+++ app.js
@@ -1,22 +1,113 @@
-// Express
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
-const path = require('path')
-PORT        = 59432;                 // Set a port number at the top so it's easy to change in the future
+// Express Setup
+var express = require('express');
+var app     = express();
+const path  = require('path')
+PORT        = 59432;
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-// Database
-//var db = require('./db-connector')
+// Handlebars Setup
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');
+app.engine('.hbs', engine({extname: ".hbs"}));
+app.set('view engine', '.hbs');
 
-// Route Handler
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.static('public'));
+// Database Connection
+var db = require('./database/db-connector')
+
+// -------------------------------------------------
+//                  Route Handlers
+// -------------------------------------------------
+
+// GET requests ------------------------
+
+// Index Page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/index', (req, res) => {
+    res.render('index');
+});
+
+// Pokemon Page
+app.get('/pokemon', (req, res) => {
+    let loadPokemon = "SELECT * FROM Pokemon;"
+    db.pool.query(loadPokemon, (error, rows, fields) => {
+        res.render('pokemon', {data: rows})
+    });
+});
+
+// Attacks Page
+app.get('/attacks', (req, res) => {
+    res.render('attacks');
+});
+
+// Types Page
+app.get('/types', (req, res) => {
+    res.render('types');
+});
+
+// Generations Page
+app.get('/generations', (req, res) => {
+    res.render('generations');
+});
+
+// Pokemon Attacks Page
+app.get('/pokemon_attacks', (req, res) => {
+    res.render('pokemon_attacks');
+});
+
+// POST requests ------------------------
+
+// Add Pokemon
+app.post('/pokemon', (req, res) => {
+    // Collect the data
+    let data = req.body;
+    console.log(data)
+
+    let name = data['input-name'];
+    let type1 = parseInt(data['input-type2']);
+    let type2 = parseInt(data['input-type2']);
+    let gen = parseInt(data['input-gen']);
+    let preEvolution = parseInt(data['input-preEvolution']);
+    let postEvolution = parseInt(data['input-postEvolution']);
+
+    // convert NULL values to strings
+    if (isNaN(type2)) {
+        type2 = 'NULL'
+    }
+    if (isNaN(preEvolution)) {
+        preEvolution = 'NULL'
+    }
+    if (isNaN(postEvolution)) {
+        postEvolution = 'NULL'
+    }
+
+    // add the pokemon
+    query1 = `INSERT INTO Pokemon (pokemonName, typeID1, typeID2, genID, preEvolution, postEvolution)
+                VALUES ('${name}', ${type1}, ${type2}, ${gen}, ${preEvolution}, ${postEvolution});`
+    db.pool.query(query1, (error, rows, fields) => {
+        // check for errors
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        }
+        else {
+            res.redirect('/pokemon')
+        }
+    })
+});
+
+// UPDATE requests ------------------------
+
+// DELETE requests ------------------------
+
 
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(PORT, () => {
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
-});
+    });
